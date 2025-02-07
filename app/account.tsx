9c9e2826ec403d3207441665afa685baa929b28f
tanmay <kuamrchaurasiatanmay@gmail.com>
2025-02-08 02:24:35 +0530

    create simple routing

---
+++ app/account.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native"
+
+
+export default function account () {
+    return <View>
+        <Text>account page</Text>
+    </View>
+}
---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native"
+
+
+export default function foryou (){
+    return <View>
+        <Text>for you page</Text>
+    </View>
+}
